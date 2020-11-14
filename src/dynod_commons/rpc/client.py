b917469cace0c89a2270b51d081cd84d1fa9afaa
David Pochet <david.pochet@gmail.com>
2020-11-14 16:55:23 +0100

    Better RPC implem: * trace + timeout + retry on client side * trace + manager routing on server side

--- src/dynod_commons/rpc/client.py
+++ src/dynod_commons/rpc/client.py
@@ -2,10 +2,55 @@ import logging
 import os
 import pwd
 import socket
+import time
 
-from grpc import insecure_channel
+from grpc import RpcError, StatusCode, insecure_channel
+
+from dynod_commons.api import InfoApiVersion
+from dynod_commons.api.info_pb2_grpc import InfoServiceStub
+from dynod_commons.rpc.trace import trace_rpc
 
 LOG = logging.getLogger(__name__)
+
+
+# Utility class to handle stub retry
+# i.e. permissive stub that allows server to be temporarily unavailable
+class RetryStub:
+    class RetryMethod:
+        def __init__(self, name: str, stub, timeout: float, metadata: tuple):
+            self.m_name = name
+            self.s_name = stub.__class__.__name__
+            self.stub = stub
+            self.timeout = timeout
+            self.metadata = metadata
+
+        def retry_call(self, request):
+            LOG.debug(trace_rpc(True, request, method=f"{self.s_name}.{self.m_name}"))
+            first_try = time.time()
+
+            # Loop to handle retries
+            while True:
+                try:
+                    # Call real stub method, with metadata
+                    result = getattr(self.stub, self.m_name)(request, metadata=self.metadata)
+                    break
+                except RpcError as e:
+                    if e.code() == StatusCode.UNAVAILABLE and self.timeout is not None and (time.time() - first_try) < self.timeout:
+                        # Server is not available, and timeout didn't expired yet: sleep and retry
+                        time.sleep(0.5)
+                        LOG.debug(f"<RPC> >> {self.s_name}.{self.m_name}... (retry)")
+                    else:
+                        # Timed out or any other reason: raise exception
+                        LOG.error(f"<RPC> >> {self.s_name}.{self.m_name} error: {str(e)}")
+                        raise e
+
+            LOG.debug(trace_rpc(False, result, method=f"{self.s_name}.{self.m_name}"))
+            return result
+
+    def __init__(self, real_stub, timeout: float, metadata: tuple):
+        # Fake the stub methods
+        for n in filter(lambda x: not x.startswith("__") and callable(getattr(real_stub, x)), dir(real_stub)):
+            setattr(self, n, RetryStub.RetryMethod(n, real_stub, timeout, metadata).retry_call)
 
 
 class RpcClient:
@@ -18,25 +63,32 @@ class RpcClient:
         port:
             TCP port for RPC server to connect to.
         stubs_map:
-            name:stub item maps, used to instantiate and attach (generated) stubs to current client instance
-            e.g. if a {"foo": FooServiceStub} map is provided, methods of the Foo service can be accessed through a client.foo.xxx call
+            name:(stub,api_version) item maps, used to instantiate and attach (generated) stubs to current client instance
+            e.g. if a {"foo": (FooServiceStub,1)} map is provided, methods of the Foo service can be accessed through a client.foo.xxx call
+        timeout:
+            timeout for unreachable server (in seconds; default: 60)
+        name:
+            client name, which will appear in server logs
     """
 
-    def __init__(self, host: str, port: int, stubs_map: dict = None, client_api_version: int = None, timeout: float = None, name: str = None):
+    def __init__(self, host: str, port: int, stubs_map: dict, timeout: float = 60, name: str = ""):
         # Prepare metadata for RPC calls
-        self.metadata = self.get_metadata(name)
+        shared_metadata = [("client", name), ("user", self.get_user()), ("host", socket.gethostname()), ("ip", self.get_ip())]
 
         # Create channel
         LOG.debug(f"Initializing RPC client for {host}:{port}")
         channel = insecure_channel(f"{host}:{port}")
 
         # Handle stubs hooking
-        all_stubs = dict(stubs_map) if stubs_map is not None else {}
-        for name, typ in all_stubs.items():
-            LOG.debug(f"Adding {name} stub to client")
-            setattr(self, name, typ(channel))
-
-        # TODO Handle API check if client version is provided
+        all_stubs = {"info": (InfoServiceStub, InfoApiVersion.INFO_API_CURRENT)}
+        all_stubs.update(stubs_map)
+        for name, typ_n_ver in all_stubs.items():
+            typ, ver = typ_n_ver
+            metadata = list(shared_metadata)
+            if ver is not None:
+                metadata.append(("api_version", str(ver)))
+            LOG.debug(f"Adding {name} stub to client (api version: {ver})")
+            setattr(self, name, RetryStub(typ(channel), timeout, tuple(metadata)))
 
         LOG.debug(f"RPC client ready for {host}:{port}")
 
@@ -61,7 +113,3 @@ class RpcClient:
             # Not in pwd database, just keep UID
             user = f"{uid}"
         return user
-
-    def get_metadata(self, name: str):
-        # Resolve all metadata
-        return (("client", name if name is not None else ""), ("user", self.get_user()), ("host", socket.gethostname()), ("ip", self.get_ip()))
--- src/tests/test_rpc_server.py
+++ src/tests/test_rpc_server.py
@@ -1,22 +1,57 @@
 import logging
 
+import pytest
+from grpc import RpcError, StatusCode
 from pytest_multilog import TestHelper
 
-from dynod_commons.api import Empty, LoggerStatus, ResultCode
+import dynod_commons
+from dynod_commons.api import Empty, InfoApiVersion, LoggerConfig, LoggerStatus, LogsApiVersion, ResultCode, ServiceInfo
 from dynod_commons.api.logs_pb2_grpc import LogsServiceServicer, LogsServiceStub, add_LogsServiceServicer_to_server
 from dynod_commons.rpc import RpcClient, RpcServer
 from dynod_commons.utils import DynodError
 
 
 class SampleLog(LogsServiceServicer):
-    def list(self, request: Empty, context) -> LoggerStatus:  # NOQA: A003
+    def list(self, request: Empty) -> LoggerStatus:  # NOQA: A003
         logging.info("In SampleLog.list!!!")
         return LoggerStatus()
 
+    def update(self, request) -> LoggerStatus:
+        # Error sample
+        raise DynodError("sample error", rc=ResultCode.ERROR_RESOURCE_UNKNOWN)
+
 
 class TestRpcServer(TestHelper):
-    def sample_register(self, server):
-        add_LogsServiceServicer_to_server(SampleLog(), server)
+    def sample_register(self) -> list:
+        return [
+            (
+                ServiceInfo(
+                    name=LogsServiceServicer.__name__,
+                    version="0.0",
+                    current_api_version=LogsApiVersion.LOGS_API_CURRENT,
+                    supported_api_version=LogsApiVersion.LOGS_API_SUPPORTED,
+                ),
+                add_LogsServiceServicer_to_server,
+                LogsServiceServicer(),
+                SampleLog(),
+            )
+        ]
+
+    @pytest.fixture
+    def sample_server(self):
+        # Start server
+        srv = RpcServer(self.rpc_port, self.sample_register)
+
+        # Yield to test
+        yield
+
+        # Shutdown server
+        srv.shutdown()
+
+    @pytest.fixture
+    def client(self, sample_server):
+        # Setup RPC client
+        yield RpcClient("127.0.0.1", self.rpc_port, {"logs": (LogsServiceStub, LogsApiVersion.LOGS_API_CURRENT)}, name="pytest")
 
     @property
     def worker_index(self):
@@ -28,27 +63,15 @@ class TestRpcServer(TestHelper):
     def rpc_port(self) -> int:
         return 52100 + self.worker_index
 
-    def dummy_call(self):
-        # Try a dummy call
-        client = RpcClient("127.0.0.1", self.rpc_port, {"logs": LogsServiceStub})
+    def test_server(self, client):
+        # Normal call
         s = client.logs.list(Empty())
         assert s.r.code == ResultCode.OK
 
-    def test_server1(self):
-        # Start server
-        srv = RpcServer(self.rpc_port, self.sample_register)
-        self.dummy_call()
-
-        # Shutdown server
-        srv.shutdown()
-
-    def test_server2(self):
-        # Start server
-        srv = RpcServer(self.rpc_port, self.sample_register)
-        self.dummy_call()
-
-        # Shutdown server
-        srv.shutdown()
+    def test_exceptions(self, client):
+        # Error call
+        s = client.logs.update(LoggerConfig())
+        assert s.r.code == ResultCode.ERROR_RESOURCE_UNKNOWN
 
     def test_server_forbidden_port(self):
         # Try to use a system port
@@ -57,3 +80,54 @@ class TestRpcServer(TestHelper):
             raise AssertionError("Shouldn't get here")
         except DynodError as e:
             assert e.rc == ResultCode.ERROR_SUBPROCESS_FAILED
+
+    def test_get_info(self, client):
+        # Try a "get info" call
+        s = client.info.get(Empty())
+        assert len(s.items) == 2
+        info = s.items[0]
+        assert info.name == "InfoServiceServicer"
+        assert info.version == dynod_commons.__version__
+        assert info.current_api_version == InfoApiVersion.INFO_API_CURRENT
+        assert info.supported_api_version == InfoApiVersion.INFO_API_SUPPORTED
+        info = s.items[1]
+        assert info.name == "LogsServiceServicer"
+        assert info.version == "0.0"
+        assert info.current_api_version == LogsApiVersion.LOGS_API_CURRENT
+        assert info.supported_api_version == LogsApiVersion.LOGS_API_SUPPORTED
+
+    def test_client_no_version(self, sample_server):
+        # Try with client not providing API version
+        c = RpcClient("127.0.0.1", self.rpc_port, {"logs": (LogsServiceStub, None)})
+        s = c.logs.list(Empty())
+        assert s.r.code == ResultCode.OK
+
+    def test_client_too_old(self, sample_server):
+        # Try with client with too old API version
+        c = RpcClient("127.0.0.1", self.rpc_port, {"logs": (LogsServiceStub, LogsApiVersion.LOGS_API_CURRENT - 1)})
+        s = c.logs.list(Empty())
+        assert s.r.code == ResultCode.ERROR_API_CLIENT_TOO_OLD
+
+    def test_server_too_old(self, sample_server):
+        # Try with client with API version > server version
+        c = RpcClient("127.0.0.1", self.rpc_port, {"logs": (LogsServiceStub, LogsApiVersion.LOGS_API_CURRENT + 1)})
+        s = c.logs.list(Empty())
+        assert s.r.code == ResultCode.ERROR_API_SERVER_TOO_OLD
+
+    def test_no_server(self):
+        # Test behavior when client request is made and server is not ready
+        c = RpcClient("127.0.0.1", 1, {"logs": (LogsServiceStub, LogsApiVersion.LOGS_API_CURRENT)}, timeout=None)
+        try:
+            c.logs.list(Empty())
+            raise AssertionError("Shouldn't get there")
+        except RpcError as e:
+            assert e.code() == StatusCode.UNAVAILABLE
+
+    def test_no_server_timeout(self):
+        # Same as above, with timeout
+        c = RpcClient("127.0.0.1", 1, {"logs": (LogsServiceStub, LogsApiVersion.LOGS_API_CURRENT)}, timeout=1)
+        try:
+            c.logs.list(Empty())
+            raise AssertionError("Shouldn't get there")
+        except RpcError as e:
+            assert e.code() == StatusCode.UNAVAILABLE
